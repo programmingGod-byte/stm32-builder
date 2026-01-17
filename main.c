26573a35f7775cfb4dd99c0c471eb0d3f6989564
programmingGod-byte <shivam.kumar262200628@gmail.com>
2026-01-17 15:08:30 +0530

    Update main.c

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void setup() {
+    // Initialize serial communication
+}
+
+void loop() {
+    // Main logic here
+    printf("Reading sensors...\n");
+}
